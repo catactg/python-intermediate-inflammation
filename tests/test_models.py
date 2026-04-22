45b1c86d13d8e1cd7851f36d571ed294480b7583
catactg <catactg@gmail.com>
2026-04-22 12:39:02 +0200

    Add tests for data validation

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
-
+import pytest
 from inflammation.models import daily_mean, daily_min
 
 def test_daily_mean_zeros():
@@ -29,6 +29,18 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_daily_mean_string():
+    """Test that the mean function fails for an array of strings
+    """
+    with pytest.raises(TypeError):
+        error_expected = daily_mean(['hi','there'])
+
+def test_daily_mean_non_iterable():
+    """Test that the mean function fails for an array of strings
+    """
+    with pytest.raises(IndexError):
+        error_expected = daily_mean(9)
+
 def test_daily_min_integers():
     """Test that the min function works for an array of positive and negative intergers.
     """
@@ -39,3 +51,9 @@ def test_daily_min_integers():
     test_result = np.array([1, -9, -1])
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_min_string():
+    """Test that the min function fails for an array of strings
+    """
+    with pytest.raises(TypeError):
+        error_expected = daily_min(['hi','there'])
