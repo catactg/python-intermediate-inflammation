392fc2fd2206eb1d7b09df272fea3891461042c2
catactg <catactg@gmail.com>
2026-05-04 15:21:11 +0200

    fixing docstrings

--- inflammation/models.py
+++ inflammation/models.py
@@ -18,18 +18,30 @@ def load_csv(filename):
     return np.loadtxt(fname=filename, delimiter=',')
 
 
-def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
+def daily_mean(data: np.ndarray) -> np.ndarray:
+    """Calculate the daily mean of a 2D inflammation data array for each day.
+
+    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :return: An array of mean values of measurements for each day.
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array."""
+    """Calculate the daily maximum of a 2D inflammation data array for each day.
+
+    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :return: An array of max values of measurements for each day.
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array."""
+    """Calculate the daily minimum of a 2D inflammation data array for each day.
+
+    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :return: An array of minimum values of measurements for each day.
+    """
     return np.min(data, axis=0)
 
 
