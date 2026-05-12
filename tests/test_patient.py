7b40c9d294cffc90fdd2634ed59f1a62a28f32a7
catactg <catactg@gmail.com>
2026-05-12 15:58:11 +0200

    Add a unit test for `Patient.get_body_mass_index`

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -1,6 +1,7 @@
 """Tests for the Patient model."""
 
 from inflammation.models import Patient
+import numpy.testing as npt
 
 def test_create_patient():
     name = 'Alice'
@@ -11,3 +12,8 @@ def test_create_patient():
     assert p.name == name
     assert p.weight == w        # good practice: test the new properties
     assert p.height == h        # good practice: test the new properties
+
+def test_compute_bmi():
+    maria = Patient(name='maria',  height=1.6, weight=60)
+    expected_bmi = 23.4375
+    npt.assert_almost_equal(maria.get_body_mass_index(), expected_bmi)
