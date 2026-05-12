50dd38680560586eec914d159391ec816c781bfd
catactg <catactg@gmail.com>
2026-05-12 15:30:42 +0200

    Add data validation unit tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,6 +29,13 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_daily_mean_string():
+    """Test that the mean function fails for an array of strings
+    """
+    with pytest.raises(TypeError):
+        error_expected = daily_mean(['hi','there'])
+
 def test_daily_max_integers():
     """Test that the max function works for an array of positive integers.
     """
@@ -53,3 +60,9 @@ def test_daily_min_integers():
     test_result = np.array([-3, 2, -9])              
 
     npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_mean_non_iterable():
+    """Test that the mean function fails for a non iterable (single integer)
+    """
+    with pytest.raises(IndexError):
+        error_expected = daily_mean(9)
