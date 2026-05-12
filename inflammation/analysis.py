6e2bbf0058dfb9c8d85d7c4e56ef47933f6c57c9
catactg <catactg@gmail.com>
2026-05-12 15:50:28 +0200

    Decouple Data Loading from Data Analysis

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -1,5 +1,4 @@
-"""Module containing mechanism for calculating standard deviation between datasets.
-"""
+"""Module containing mechanism for calculating standard deviation between datasets."""
 
 import glob
 import os
@@ -14,11 +13,8 @@ def analyse_data(data_dir):
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
 
+    data = load_inflammation_data(data_dir=data_dir)
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
@@ -26,3 +22,11 @@ def analyse_data(data_dir):
     daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
 
     return daily_standard_deviation
+
+
+def load_inflammation_data(data_dir):
+    data_file_paths = glob.glob(os.path.join(data_dir, "inflammation*.csv"))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+    data = map(models.load_csv, data_file_paths)
+    return list(data)
