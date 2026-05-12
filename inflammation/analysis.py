1592f76590b51dd017ae0e3bd77cfa093aff4c41
catactg <catactg@gmail.com>
2026-05-12 16:12:25 +0200

    Refactoring To Use a Pure Function

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -6,34 +6,43 @@ import numpy as np
 
 from inflammation import models, views
 
+
 class CSVDataSource:
     """
     Loads all the inflammation CSV files within a specified directory.
     """
+
     def __init__(self, data_dir):
         self.data_dir = data_dir
 
     def load_inflammation_data(self):
-        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.csv'))
+        data_file_paths = glob.glob(os.path.join(self.data_dir, "inflammation*.csv"))
         if len(data_file_paths) == 0:
-            raise ValueError(f"No inflammation data CSV files found in path {self.data_dir}")
+            raise ValueError(
+                f"No inflammation data CSV files found in path {self.data_dir}"
+            )
         data = map(models.load_csv, data_file_paths)
         return list(data)
-    
+
+
 class JSONDataSource:
     """
     Loads all the inflammation JSON files within a specified directory.
     """
+
     def __init__(self, data_dir):
         self.data_dir = data_dir
 
     def load_inflammation_data(self):
-        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.json'))
+        data_file_paths = glob.glob(os.path.join(self.data_dir, "inflammation*.json"))
         if len(data_file_paths) == 0:
-            raise ValueError(f"No inflammation data JSON files found in path {self.data_dir}")
+            raise ValueError(
+                f"No inflammation data JSON files found in path {self.data_dir}"
+            )
         data = map(models.load_csv, data_file_paths)
         return list(data)
-    
+
+
 def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
@@ -42,10 +51,14 @@ def analyse_data(data_source):
     then plots the graphs of standard deviation of these means."""
 
     data = data_source.load_inflammation_data()
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
 
+    return daily_standard_deviation
+
+
+def compute_standard_deviation_by_day(data):
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
 
     daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
-
     return daily_standard_deviation
