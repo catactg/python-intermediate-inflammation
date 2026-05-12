342fd47538979b885f028f2b56087fe83f5828cc
catactg <catactg@gmail.com>
2026-05-12 16:15:41 +0200

    Use optional input parameter to save figures

--- inflammation/views.py
+++ inflammation/views.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def visualize(data_dict):
+def visualize(data_dict, outfile):
     """Display plots of basic statistical properties of the inflammation data.
 
     :param data_dict: Dictionary of name -> data to plot
@@ -20,6 +20,8 @@ def visualize(data_dict):
         axes.set_ylabel(name)
         axes.plot(data)
 
-    fig.tight_layout()
-
-    plt.show()
+    if outfile:
+        plt.savefig(outfile)
+    else:
+        fig.tight_layout()          # this command triggers the show
+        plt.show()
